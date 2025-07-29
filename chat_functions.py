34028753a2f57ecdb408b9d0e0f46a1d1d4740d2
Carlos Henrique <henriqueetec1987@gmail.com>
2025-07-29 19:49:17 -0300

    add function to generate content

--- chat_functions.py
+++ chat_functions.py
@@ -1,11 +1,13 @@
 import google.generativeai as genai
 import os
+from fpdf import FPDF
+
 
 # This function allows you to create multiple prompts, a concept for prompt engineering
 def initial_prompts():
     prompts = [
      "você é um analista de dados sua tarefa é responder e preencher o documento de proposta de trabalho de acordo com a metodologia PACE, será solicitado qual é stage pace este contexto faz parte, seu papel é responder e informar se necessario qual função é designada para isso",
-     "voce é um torcedor do sao paulo futebol clube"
+     "Voce é um Torcedor do sao paulo futebol clube, disserte sobre seu clube"
     ]
     return prompts    
 
@@ -13,7 +15,15 @@ def initial_prompts():
 def initial_setting():
     prompts = initial_prompts()
     genai.configure(api_key=os.environ["KEY_GEMINI"])
-    print(f"seu prompt inicial é este: {prompts[0]}")
-    model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=prompts[0])
+    model = genai.GenerativeModel("gemini-1.5-flash", system_instruction=prompts[1])
     return model
+
+# Generates AI content using Gemini model, prints, then returns text.
+def generate_content():
+    model = initial_setting()    
+    content = "Ola,"
+    response = model.generate_content(content)
+    print(response.text)
+    return response.text
+
 
